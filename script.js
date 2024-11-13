4e97d7691c5cb8a69d5a70140255febd13090f1f
Francesca-Lisi <francescalisi2143@gmail.com>
2024-11-13 20:47:08 +0100

    feat: add initial datas and prompts in script

---
+++ script.js
@@ -0,0 +1,10 @@
+//DATI INIZIALI
+const price = 0.21 //€ al km
+const discountKid = 0.2;
+const discountSenior = 0.4;
+
+//DATI RICHIESTI ALL'UTENTE
+const lengthKilometers = parseInt(prompt ('Inserisci i kilometri che vuoi percorrere'));
+const agePassenger = parseInt(prompt ('Inserisci la tua età'));
+
+
